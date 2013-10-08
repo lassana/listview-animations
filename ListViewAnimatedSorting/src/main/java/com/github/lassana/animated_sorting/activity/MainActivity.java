78bb196390295e5ef63ebe3bc341b502637e329f
lassana <lassana.nd@gmail.com>
2013-10-08 17:51:08 +0300

    Move 'MainActivity' to package 'com.github.lassana.animated_sorting.activity'.

--- ListViewAnimatedSorting/src/main/java/com/github/lassana/animated_sorting/activity/MainActivity.java
+++ ListViewAnimatedSorting/src/main/java/com/github/lassana/animated_sorting/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.github.lassana.animated_sorting;
+package com.github.lassana.animated_sorting.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+
+import com.github.lassana.animated_sorting.R;
 
 /**
  * @author lassana
