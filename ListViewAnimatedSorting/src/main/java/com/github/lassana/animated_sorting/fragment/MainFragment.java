3d747b74a4946530c0954146ea4342da1896d433
lassana <lassana.nd@gmail.com>
2013-10-08 18:04:19 +0300

    Add little listview to fragment.

--- ListViewAnimatedSorting/src/main/java/com/github/lassana/animated_sorting/fragment/MainFragment.java
+++ ListViewAnimatedSorting/src/main/java/com/github/lassana/animated_sorting/fragment/MainFragment.java
@@ -5,8 +5,12 @@ import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import com.github.lassana.animated_sorting.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * @author lassana
@@ -17,5 +21,15 @@ public class MainFragment extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        final String[] cars = { "Volvo", "Mercedes", "Audi", "Land Rover", "BMW", "Ford", "GMC", "Mazda",
+                "Acura", "Vaz", "Renault"};
+        final ArrayList<String> carsArrayList = new ArrayList<String>(Arrays.asList(cars));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, carsArrayList);
+        setListAdapter(adapter);
     }
 }
